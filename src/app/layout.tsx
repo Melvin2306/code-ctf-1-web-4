03a43d69eb2ae90fc7398447da1c8a4cbcc482ec
Melvin2306 <melvin.rinkleff@gmx.de>
2024-02-09 13:06:41 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'CODE founder platform',
+  title: 'CODE module database',
 };
 
 export default function RootLayout({
